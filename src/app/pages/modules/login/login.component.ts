26a27e13f8321376d5e466c4fc660c417b4b16a1
manikanta <saipavankumargunda@gmail.com>
2021-12-08 11:17:59 +0530

    sample api method

--- src/app/pages/modules/login/login.component.ts
+++ src/app/pages/modules/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { first } from "rxjs/operators";
 import { HttpClient } from "@angular/common/http";
 import { from } from "rxjs";
+import { ApiService } from "../../api.service";
 
 @Component({
   selector: "app-login",
@@ -12,13 +13,15 @@ import { from } from "rxjs";
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
+  users: any;
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
     private http: HttpClient,
     private _activatedRoute: ActivatedRoute,
-    private _router: Router
+    private _router: Router,
+    private api: ApiService
   ) {}
 
   ngOnInit() {
@@ -26,6 +29,10 @@ export class LoginComponent implements OnInit {
       username: ['', Validators.required],
       password: ['', Validators.required],
       token: ['', Validators.required]
+  });
+  this.api.get('users?page=1').subscribe(res => {
+    this.users = res;
+    console.log('data response', this.users);
   });
     // get return url from route parameters or default to '/'
     // this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/home";
