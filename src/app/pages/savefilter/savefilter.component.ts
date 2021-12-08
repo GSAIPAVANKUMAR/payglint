68676b6bb255670488effe50a5c585056b7d657a
manikanta <saipavankumargunda@gmail.com>
2021-12-08 10:57:53 +0530

    changed styles in save filter model

--- src/app/pages/savefilter/savefilter.component.ts
+++ src/app/pages/savefilter/savefilter.component.ts
@@ -7,12 +7,18 @@ import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
   styleUrls: ['./savefilter.component.scss']
 })
 export class SavefilterComponent implements OnInit {
-
+  access:any = "private";
+  show:boolean = true;
+  selectedOption:any;
   constructor(private _mdr: MatDialogRef<SavefilterComponent>) { }
 
   ngOnInit(): void {
   }
   CloseDialog() {
     this._mdr.close(false);
+  }
+  selectedValue(val:any) {
+    this.show = !this.show;
+    this.access = val;
   }
 }
